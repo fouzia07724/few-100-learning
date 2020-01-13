ec825abdd08b84624557be501aa6abe59b137dd4
fouzia07724 <59837518+fouzia07724@users.noreply.github.com>
2020-01-13 20:53:06 +0000

    End of the day

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-
+import { add } from './math'
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
-
+console.log('Ready to Get Down With Some TypeScript!');
+console.log(add(3, 3));
